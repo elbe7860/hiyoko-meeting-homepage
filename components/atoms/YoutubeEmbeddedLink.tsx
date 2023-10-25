204bf9a1c555fc4ed8833b881e82416cd8325834
Kei Soga <kei.gitreply@gmail.com>
2023-10-25 22:42:04 +0900

    Fix オプションの指定がReact方式ではなかったので修正

--- components/atoms/YoutubeEmbeddedLink.tsx
+++ components/atoms/YoutubeEmbeddedLink.tsx
@@ -10,9 +10,9 @@ export default function YoutubeEmbeddedLink({
       className="w-full h-full"
       src={url}
       title="YouTube video player"
-      frameborder="0"
+      frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
+      allowFullScreen
     ></iframe>
   )
 }
